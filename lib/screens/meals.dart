f54633f525861a6502f6a111534baf660f9f410f
nIkHiL <zero.zero619.ns@gmail.com>
2023-07-06 18:56:53 +0530

    - updated meal_details.dart, meal_item.dart, meal_item_trait.dart, meals.dart and tabs.dart

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,34 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:meals_with_filter/widgets/meal_item.dart';
 
 import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+import 'meal_details.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
-    required this.title,
+    super.key,
+    this.title,
     required this.meals,
+    required this.onToggleFavorite,
   });
-  final String title;
+
+  final String? title;
   final List<Meal> meals;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          title,
+  final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
-      body: meals.isNotEmpty
-          ? ListView.builder(
-              itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
-              itemCount: meals.length)
-          : const Center(
-              child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("U h oh nooo"),
-              ],
-            )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
+
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
     );
   }
 }
